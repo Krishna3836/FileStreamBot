23632a2fdb64b1db8622974cb7b745f3230fc739
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-28 13:35:27 +0530

    Update stream.py

--- Code_X_Mania/bot/plugins/stream.py
+++ Code_X_Mania/bot/plugins/stream.py
@@ -93,7 +93,7 @@ async def private_receive_handler(c: Client, m: Message):
         
         
 
-        msg_text = "Your Link Generated! \n\n📇 File Name : `{}` \n\n🗄️ File Size :`{}` \n\n📥 Download Link `{}` \n\n🎥 Stream `{}` "
+        msg_text = "Bruh! 😁\nYour Link Generated! 🤓\n\n📂 **File Name:** `{}`\n**File Size:** `{}`\n\n📥 **Download Link:** `{}`"
 
         await log_msg.reply_text(text=f"**RᴇQᴜᴇꜱᴛᴇᴅ ʙʏ :** [{m.from_user.first_name}](tg://user?id={m.from_user.id})\n**Uꜱᴇʀ ɪᴅ :** `{m.from_user.id}`\n**Stream ʟɪɴᴋ :** {stream_link}", disable_web_page_preview=True, parse_mode="Markdown", quote=True)
         await m.reply_text(
