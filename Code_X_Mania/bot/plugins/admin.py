9892016c05192674e63ebfebe1e426d9298bf29e
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-28 13:58:35 +0530

    Update admin.py

--- Code_X_Mania/bot/plugins/admin.py
+++ Code_X_Mania/bot/plugins/admin.py
@@ -16,10 +16,7 @@ db = Database(Var.DATABASE_URL, Var.SESSION_NAME)
 broadcast_ids = {}
 
 
-@StreamBot.on_message(filters.command("status") & filters.private & filters.user(Var.OWNER_ID) & ~filters.edited)
-async def sts(c: Client, m: Message):
-    total_users = await db.total_users_count()
-    await m.reply_text(text=f"**Total Users in DB:** `{total_users}`", parse_mode="Markdown", quote=True)
+
 
 
 @StreamBot.on_message(filters.command("broadcast") & filters.private & filters.user(Var.OWNER_ID) & filters.reply & ~filters.edited)
