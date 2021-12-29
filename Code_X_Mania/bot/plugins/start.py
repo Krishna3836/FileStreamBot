899bd54bd3eed343e4eaee9378acf6935867d591
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 15:26:52 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -5,7 +5,7 @@ import logging
 logger = logging.getLogger(__name__)
 
 from Code_X_Mania.utils.human_readable import humanbytes
-from Database.add import add_user_to_database
+from Code_X_Mania.add import add_user_to_database
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from pyrogram.errors import UserNotParticipant
