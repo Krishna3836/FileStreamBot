0b57a1b6542b47040c3e3a63ae91545f8bc64c5c
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 17:08:44 +0530

    Update cbbroadcast.py

--- Code_X_Mania/bot/plugins/cbbroadcast.py
+++ Code_X_Mania/bot/plugins/cbbroadcast.py
@@ -1,1 +1,17 @@
 
+# (c) @AbirHasan2005
+
+import shutil
+import psutil
+from pyrogram import filters
+from pyrogram.types import (
+    Message
+)
+from Code_X_Mania.Var import Var
+from pyrogram import Client
+
+
+
+@Client.on_message(filters.command("broadcast") & filters.user(Var.OWNER_ID) & filters.reply & ~filters.edited)
+async def broadcast_in(_, m: Message):
+    await broadcast_handler(m)
