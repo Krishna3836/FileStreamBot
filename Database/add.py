be89480e8f7ac01793cc26dc29d4c84f7b50d261
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 14:19:22 +0530

    Update add.py

--- Database/add.py
+++ Database/add.py
@@ -1,16 +1,17 @@
 # (c) @AbirHasan2005
 
-from configs import Config
+
+from Code_X_Mania.vars import Var
 from .database import db
-from pyrogram import Client
+from Code_X_Mania.bot import StreamBot
 from pyrogram.types import Message
 
 
 async def add_user_to_database(bot: Client, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if Config.LOG_CHANNEL is not None:
+        if Var.Bin_Channel is not None:
             await bot.send_message(
-                int(Config.LOG_CHANNEL),
+                int(Var.BIN_CHANNEL),
                 f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
             )
