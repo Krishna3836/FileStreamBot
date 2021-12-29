51495daf46f181dce91d4d44a8248fa2d4e9e5f5
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 15:02:33 +0530

    Update add.py

--- Database/add.py
+++ Database/add.py
@@ -10,7 +10,7 @@ from pyrogram.types import Message
 async def add_user_to_database(bot: StreamBot, cmd: Message):
     if not await db.is_user_exist(cmd.from_user.id):
         await db.add_user(cmd.from_user.id)
-        if Var.Bin_Channel is not None:
+        if Var.BIN_CHANNEL is not None:
             await bot.send_message(
                 int(Var.BIN_CHANNEL),
                 f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
