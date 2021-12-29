88e3e991032a3bc018fc90cae39df54441f3acec
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 17:57:43 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -42,7 +42,7 @@ START_BUTTONS = InlineKeyboardMarkup(
 
 
 # Start Message
-@Client.on_message(filters.private & filters.incoming & filters.command("start"))
+@StreamBot.on_message(filters.private & filters.incoming & filters.command("start"))
 async def start(bot, msg):
 	user = await bot.get_me()
 	mention = user["mention"]
