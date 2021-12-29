5d9b5cc19db57684eb24830f3c4cda2affd541d4
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 18:06:08 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -37,9 +37,8 @@ START_BUTTONS = InlineKeyboardMarkup(
         ]]            
             
         
-
 @StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private & ~filters.edited)
-async def start(b, m):
+        async def start(b, m):
         await StreamBot.send_photo(
             chat_id=m.chat.id,
             photo ="https://user-images.githubusercontent.com/88939380/137127129-a86fc939-2931-4c66-b6f6-b57711a9eab7.png",
