d38cb90e677f773a78a55db8c8770bb4591ba169
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 18:35:15 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -70,3 +70,25 @@ async def start(b, m):
             disable_web_page_preview=True,
             reply_markup=buttonz
               )
+@StreamBot.on_callback_query()
+async def cb_data(bot, update):
+    if update.data == "home":
+        await update.message.edit_text(
+            text=START_TEXT.format(update.from_user.mention),
+            disable_web_page_preview=True,
+            reply_markup=START_BUTTONS
+        )
+    elif update.data == "help":
+        await update.message.edit_text(
+            text=HELP_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=HELP_BUTTONS
+        )
+    elif update.data == "about":
+        await update.message.edit_text(
+            text=ABOUT_TEXT,
+            disable_web_page_preview=True,
+            reply_markup=ABOUT_BUTTONS
+        )
+    else:
+        await update.message.delete()
