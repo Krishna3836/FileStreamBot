1b1aacbf0e8d81708a3d09216e3bcc570416f663
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 12:52:30 +0530

    Update broadcast_helper.py

--- Code_X_Mania/utils/broadcast_helper.py
+++ Code_X_Mania/utils/broadcast_helper.py
@@ -4,10 +4,12 @@ import asyncio
 import traceback
 from pyrogram.errors import FloodWait, InputUserDeactivated, UserIsBlocked, PeerIdInvalid
 
-
 async def send_msg(user_id, message):
     try:
-        await message.forward(chat_id=user_id)
+        if Config.BROADCAST_AS_COPY is False:
+            await message.forward(chat_id=user_id)
+        elif Config.BROADCAST_AS_COPY is True:
+            await message.copy(chat_id=user_id)
         return 200, None
     except FloodWait as e:
         await asyncio.sleep(e.x)
