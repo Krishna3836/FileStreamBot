8a16b62d4c61523ed9634a843e78565dcb0e0a33
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 18:03:07 +0530

    Update start.py

--- Code_X_Mania/bot/plugins/start.py
+++ Code_X_Mania/bot/plugins/start.py
@@ -42,7 +42,16 @@ START_BUTTONS = InlineKeyboardMarkup(
 
 
 # Start Message
-@
+@StreamBot.on_message((filters.command("start") | filters.regex('start⚡️')) & filters.private & ~filters.edited)
+async def start(b, m):
+        await StreamBot.send_photo(
+            chat_id=m.chat.id,
+            photo ="https://user-images.githubusercontent.com/88939380/137127129-a86fc939-2931-4c66-b6f6-b57711a9eab7.png",
+            caption ="""Hi !
+I am Telegram File to Link Generator Bot with Channel support.
+Send me any file and get a direct download link and streamable link.!""",
+            parse_mode="html",
+            reply_markup=START_BUTTONS)
 	await bot.send_message(
 		msg.chat.id,
 		'Use below buttons or Commands To Use Me',
