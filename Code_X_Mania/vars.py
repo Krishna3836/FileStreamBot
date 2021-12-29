50bd33a189cc037a82a95baa122f565f10cd19d1
Tellybots <93035329+Tellybots@users.noreply.github.com>
2021-12-29 17:31:26 +0530

    Update vars.py

--- Code_X_Mania/vars.py
+++ Code_X_Mania/vars.py
@@ -13,10 +13,10 @@ class Var(object):
     SESSION_NAME = str(getenv('SESSION_NAME', 'AHFile2LinkBot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL'))
+    
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
-    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    
 
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
@@ -26,9 +26,15 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU else APP_NAME+'.herokuapp.com'
-    MONGODB_URI = str(getenv('MONGODB_URI'))
+    
     PING_INTERVAL = int(getenv('PING_INTERVAL', '500'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split()))
     URL = "http://{}/".format(FQDN)
-    BROADCAST_AS_COPY = str(os.environ.get("BROADCAST_AS_COPY", "False"))
+    
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
+    PRO_USERS.append(OWNER_ID)
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    BIN_CHANNEL = int(os.environ.get("BIN_CHANNEL", "-100"))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
